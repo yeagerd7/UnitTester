29664565c4de991b235879309e4619c0846bdf5a
yeagerd7 <31807185+yeagerd7@users.noreply.github.com>
2017-10-05 23:02:34 -0400

    Added an Alert Box Generator Class

---
+++ src/sample/AlertBox.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.event.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
+import javafx.scene.control.*;
+
+public class AlertBox {
+
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL); //Doesn't allow user interaction with other windows
+        window.setTitle(title); //Sets the window title text desired via the parameter
+        window.setMinWidth(300);
+
+        Label label = new Label();
+        label.setText(message); //Sets the label text desired via the parameter
+
+        Button closeButton = new Button();
+        closeButton.setText("Ok");
+        closeButton.setOnAction(e -> window.close()); //Lambda pointing event handler to close window on click
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        window.setScene(new Scene(layout, 300, 100));
+        window.show();
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,24 +8,25 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.*;
 import javafx.scene.control.*;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
 
     Button browseButton;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) {launch(args);}
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         //Build Main Window
         primaryStage.setTitle("C++ Unit Tester");
 
         //Browse Button
         browseButton = new Button();
         browseButton.setText("Browse");
-        browseButton.setOnAction(this);
+        browseButton.setOnAction(e -> {
+            AlertBox.display("ERROR", "Error Message: Click 'Ok' to continue");
+        });   //Incomplete: Expecting a Method Call for Browse Functionality
 
         StackPane layout = new StackPane();
         layout.getChildren().add(browseButton);
@@ -33,11 +34,5 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         //Makes window visible
         primaryStage.setScene(new Scene(layout, 700, 600));
         primaryStage.show();
-    }
-
-
-    @Override
-    public void handle(ActionEvent event) {
-
     }
 }
