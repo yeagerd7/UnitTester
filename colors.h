78353fed7ae08d48a8cb1d047efc0568c44635c6
EricLynn <32644694+EricLynn@users.noreply.github.com>
2017-11-12 15:50:33 -0500

    Add files via upload
    Example C++ Project for testing purposes

---
+++ colors.h
@@ -0,0 +1,13 @@
+#ifndef _COLORS_H
+#define _COLORS_H
+
+#define RESET      "\x1B[0m"
+#define SETRED     "\x1B[31m"
+#define SETGREEN   "\x1B[32m"
+#define SETYELLOW  "\x1B[33m"
+#define SETBLUE    "\x1B[34m"
+#define SETMAGENTA "\x1B[35m"
+#define SETCYAN    "\x1B[36m"
+#define SETWHITE   "\x1B[37m"
+
+#endif // _COLORS_H
